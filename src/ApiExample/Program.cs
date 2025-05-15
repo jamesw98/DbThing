e2bcf8d576d0782d36c86019897ca1cb0c89eeef
jamesw98 <xeekatar@gmail.com>
2025-05-14 23:55:39 -0400

    updated example api with new example

--- src/ApiExample/Program.cs
+++ src/ApiExample/Program.cs
@@ -1,12 +1,15 @@
+using System.Diagnostics;
 using ApiExample;
 using DbThing;
-using Microsoft.Data.SqlClient;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Host.UseSerilog((_, loggerConfig) => loggerConfig.ReadFrom.Configuration(builder.Configuration));
+
 
 var app = builder.Build();
 
@@ -20,11 +23,21 @@ app.UseHttpsRedirection();
 
 var repo = new DbRepository(builder.Configuration);
 
-app.MapGet("/person", async () =>
-    {
-        var results = await repo.QueryAsync<Person>("usp_get_employees");
-        return results;
-    })
-.WithName("GetPeople");
+app.MapGet("/person", async () => await (GetListHelper<Person>("usp_get_employees", "/person")))
+    .WithName("GetPeople");
+
+app
+    .MapGet("/order", async () => await (GetListHelper<Order>("usp_get_orders_with_product_details", "/order")))
+    .WithName("GetOrders");
 
 app.Run();
+return;
+
+async Task<List<T>> GetListHelper<T>(string proc, string endpoint) where T : IDbModel, new()
+{
+    var sw = Stopwatch.StartNew();
+    var results = await repo.QueryAsync<T>(proc);
+    sw.Stop();
+    Log.Information("Finished call to {EndPoint}, took {Time} seconds.", endpoint, sw.Elapsed);
+    return results;
+}
