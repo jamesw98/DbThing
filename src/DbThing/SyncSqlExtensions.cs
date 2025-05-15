59a5085303b0de7dfda5f979e06ddf607d2a65a5
jamesw98 <xeekatar@gmail.com>
2025-05-15 00:05:58 -0400

    re-orged some files

---
+++ src/DbThing/SyncSqlExtensions.cs
@@ -0,0 +1,102 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace DbThing;
+
+public static class SyncSqlExtensions
+{
+    #region Sync
+    
+    /// <summary>
+    /// Executes a stored procedure that is not expected to return any data.
+    /// </summary>
+    /// <param name="connection">The connection to use.</param>
+    /// <param name="procedure">The procedure to be run.</param>
+    /// <param name="parameters">The parameters to pass to the procedure.</param>
+    public static void Execute
+    (
+        this SqlConnection connection,
+        string procedure,
+        params SqlParameter[] parameters
+    )
+    {
+        var (transaction, command) = Utils.Setup(connection, procedure, parameters);
+        Utils.Run(connection, transaction, command, procedure, () => command.ExecuteNonQuery());
+    }
+    
+    /// <inheritdoc cref="AsyncSqlExtensions.QueryAsync{T}"/>
+    public static List<T> Query<T>
+    (
+        this SqlConnection connection,
+        string procedure,
+        params SqlParameter[] parameters
+    ) where T : IDbModel, new()
+    {
+        var (transaction, command) = Utils.Setup(connection, procedure, parameters);
+
+        return Utils.Run(connection, transaction, command, procedure,() =>
+        {
+            using var reader = command.ExecuteReader();
+
+            var result = new List<T>();
+            while (reader.Read())
+            {
+                var valuesForKeys = new Dictionary<string, object>();
+                var values = new object[reader.FieldCount];
+                reader.GetValues(values);
+
+                for (var i = 0; i < values.Length; i++)
+                {
+                    valuesForKeys.Add(reader.GetName(i), values[i]);
+                }
+
+                var item = new T();
+                item.Initialize(valuesForKeys);
+                result.Add(item);
+            }
+
+            return result;
+        });
+    }
+    
+    /// <inheritdoc cref="AsyncSqlExtensions.QuerySingleColumnListAsync{T}"/>
+    public static List<T?> QuerySingleColumn<T>
+    (
+        this SqlConnection connection,
+        string procedure,
+        string columnName,
+        params SqlParameter[] parameters
+    )
+    {
+        connection.Open();
+
+        using var transaction = connection.BeginTransaction();
+        using var command = new SqlCommand(procedure, connection, transaction);
+        command.Parameters.AddRange(parameters);
+        command.CommandType = CommandType.StoredProcedure;
+
+        using var reader = command.ExecuteReader();
+
+        var resultValues = new List<T?>();
+        while (reader.Read())
+        {
+            try
+            {
+                var ordinal = reader.GetOrdinal(columnName);
+                var result = reader.GetValue(ordinal);
+                resultValues.Add(result.TryCast<T>());
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                // Throw a more human-readable exception.
+                throw new IndexOutOfRangeException($"Could not find column {columnName} in procedure result", e);
+            }
+        }
+        
+        transaction.Commit();
+        connection.Close();
+        return resultValues;
+    }
+    
+    #endregion
+}
---
+++ src/DbThing/Utils.cs
@@ -0,0 +1,121 @@
+﻿using System.Data;
+using System.Diagnostics;
+using Microsoft.Data.SqlClient;
+using Serilog;
+
+namespace DbThing;
+
+public static class Utils
+{
+    /// <summary>
+    /// Sets up a query to be run.
+    /// </summary>
+    /// <param name="connection">The connection to use.</param>
+    /// <param name="procedure">The procedure to run.</param>
+    /// <param name="parameters">The parameters to send to the procedure.</param>
+    /// <returns>A tuple containing a transaction and the sql command.</returns>
+    public static (SqlTransaction transaction, SqlCommand command) Setup
+    (
+        SqlConnection connection, 
+        string procedure,
+        SqlParameter[] parameters
+    )
+    {
+        // Open a connection, start a transaction, and create a new SQL command with the appropriate params.
+        connection.Open();
+        var transaction = connection.BeginTransaction();
+        var command = new SqlCommand(procedure, connection, transaction);
+        
+        // Check all the procedure parameters to see if we have a default or a null value. If we do, convert the 
+        // parameter to DBNull. This prevents a bunch of strange things from happening.
+        foreach (var p in parameters)
+        {
+            if (p is { IsNullable: true, Value: null } || p.Value is null)
+            {
+                p.SqlValue = DBNull.Value;
+            }
+        }
+
+        // Add the params and command type to the SQL command.        
+        command.Parameters.AddRange(parameters);
+        command.CommandType = CommandType.StoredProcedure;
+        return (transaction, command);
+    }
+
+    /// <summary>
+    /// Run a stored procedure. This handles commiting or rolling back the transaction depending on the result of the
+    /// query.
+    /// </summary>
+    /// <param name="connection">The DB connection to use.</param>
+    /// <param name="transaction">The transaction to use.</param>
+    /// <param name="command">The command to run.</param>
+    /// <param name="procedure">The name of the proc being run.</param>
+    /// <param name="action">The action to run the command and do any parsing.</param>
+    /// <typeparam name="T">The type we want to return from the procedure.</typeparam>
+    /// <returns>The result of the procedure.</returns>
+    public static async Task<T> RunAsync<T>
+    (
+        SqlConnection connection,
+        SqlTransaction transaction,
+        SqlCommand command,
+        string procedure,
+        Func<Task<T>> action
+    )
+    {
+        try
+        {
+            var sw = Stopwatch.StartNew();
+            var result = await action.Invoke();
+            transaction.Commit();
+            sw.Stop();
+            Log.Information("Executed procedure {Procedure}. Took {Time}ms", procedure, 
+                sw.Elapsed.Milliseconds);
+            return result;
+        }
+        catch (Exception e)
+        {
+            Log.Error("Procedure {Procedure} encountered an error:\n{Error}", procedure, e.Message);
+            transaction.Rollback();
+            throw;
+        }
+        finally
+        {
+            command.Dispose();
+            connection.Close();
+        }
+    }
+    
+    /// <inheritdoc cref="RunAsync{T}"/>
+    public static T Run<T>
+    (
+        SqlConnection connection,
+        SqlTransaction transaction,
+        SqlCommand command,
+        string procedure,
+        Func<T> action
+    )
+    {
+        try
+        {
+            var sw = Stopwatch.StartNew();
+            var result = action.Invoke();
+            transaction.Commit();
+            sw.Stop();
+            Log.Information("Executed procedure {Procedure}. Took {Time}ms", procedure, 
+                sw.Elapsed.Milliseconds);
+            return result;
+        }
+        catch (Exception e)
+        {
+            Log.Error("Procedure {Procedure} encountered an error:\n{Error}",
+                procedure, e.Message);
+            transaction.Rollback();
+            throw;
+        }
+        finally
+        {
+            command.Dispose();
+            connection.Close();
+        }
+    }
+}
